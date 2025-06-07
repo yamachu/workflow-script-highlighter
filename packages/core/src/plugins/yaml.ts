0ead05e39f43c98773068ca227af1c0d6e1808c9
yamachu <yamachu.dev@gmail.com>
2025-06-07 14:18:40 +0900

    yamlも追加

---
+++ packages/core/src/plugins/yaml.ts
@@ -0,0 +1,47 @@
+import { forEachEmbeddedCode, LanguagePlugin } from "@volar/language-core";
+import type { TypeScriptExtraServiceScript } from "@volar/typescript";
+import type * as ts from "typescript";
+import { URI } from "vscode-uri";
+import { GitHubScriptVirtualCode } from "../virtualCodes/githubScript";
+
+const LANGUAGE_ID = "yaml";
+
+export const yamlLanguagePlugin: LanguagePlugin<URI> = {
+  getLanguageId(uri) {
+    if (uri.path.endsWith(".yml") || uri.path.endsWith(".yaml")) {
+      return LANGUAGE_ID;
+    }
+  },
+  createVirtualCode(_uri, languageId, snapshot) {
+    if (languageId === LANGUAGE_ID) {
+      return new GitHubScriptVirtualCode(snapshot, LANGUAGE_ID);
+    }
+  },
+  typescript: {
+    extraFileExtensions: [],
+    getServiceScript() {
+      return undefined;
+    },
+    getExtraServiceScripts(fileName, root) {
+      const scripts: TypeScriptExtraServiceScript[] = [];
+      for (const code of forEachEmbeddedCode(root)) {
+        if (code.languageId === "javascript") {
+          scripts.push({
+            fileName: fileName + "." + code.id + ".js",
+            code,
+            extension: ".js",
+            scriptKind: 1 satisfies ts.ScriptKind.JS,
+          });
+        } else if (code.languageId === "typescript") {
+          scripts.push({
+            fileName: fileName + "." + code.id + ".ts",
+            code,
+            extension: ".ts",
+            scriptKind: 3 satisfies ts.ScriptKind.TS,
+          });
+        }
+      }
+      return scripts;
+    },
+  },
+};
